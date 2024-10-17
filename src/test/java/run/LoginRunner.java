068409d5f3c060cfd96c67ea5302e41c5241d7ae
bisam-27 <f223623@cfd.nu.edu.pk>
2024-10-17 16:43:51 +0500

    Instagram

---
+++ src/test/java/run/LoginRunner.java
@@ -0,0 +1,17 @@
+package run;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/feature/login.feature",
+    glue = {"stepdefinitions"},
+    plugin = {"pretty", "html:target/cucumber-reports"},
+    monochrome = true,
+    publish = true
+)
+public class LoginRunner {
+    // Empty class, Cucumber handles the execution
+}
